98bae30bac0540453390fcbd479add4d254da007
Rohit Duhan <rohitduhan2301@gmail.com>
2022-02-16 00:36:07 +0530

    feb_22_h_hindex

--- Google_kickstart/feb_pract_sessioni_1/h_index.cpp
+++ Google_kickstart/feb_pract_sessioni_1/h_index.cpp
@@ -1,11 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> GetHIndexScore(vector<int> citations_per_paper)
+vector<int> GetHIndexScore(vector<int> cit)
 {
-    vector<int> h_index(citations_per_paper.size());
+    int n = cit.size();
+    vector<int> hind(n, 0);
 
-    return h_index;
+    map<int, int> freq;
+
+    for (int i = 0; i < n; i++)
+    {
+        freq[cit[i]]++;
+        if (i == 0)
+        {
+            hind[0] = 1;
+            continue;
+        }
+
+        hind[i] = hind[i - 1];
+        int count = hind[i] + 1;
+        int curcnt = 0;
+        auto begin = freq.begin();
+        int f = 0;
+        for (auto k = freq.begin(); k != freq.end(); k++)
+        {
+            if (k->first >= count)
+            {
+                begin = k;
+                f = 1;
+                break;
+            }
+        }
+        if (!f)
+        {
+            begin = freq.end();
+        }
+
+        for (auto j = begin; j != freq.end(); j++)
+        {
+            curcnt += j->second;
+            if (curcnt >= count)
+            {
+                hind[i]++;
+                break;
+            }
+        }
+    }
+
+    return hind;
 }
 
 int main()
@@ -24,7 +66,7 @@ int main()
             cin >> citations[i];
         }
         vector<int> answer = GetHIndexScore(citations);
-        cout << "Case #" << test_case << ": ";
+        cout << "\nCase #" << test_case << ": ";
         for (int i = 0; i < answer.size(); i++)
         {
             cout << answer[i] << " ";
